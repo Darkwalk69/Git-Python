e3c7252c6dbd5cb6eec829830eba22b6da1cd1eb
Jeff <pctech5510@gmail.com>
2025-01-01 02:21:48 -0500

    Updated

--- Dev/test.py
+++ Dev/test.py
@@ -1,3 +1,5 @@
+import DEV_HackingGameFunctions
+import DEV_HackingGame
 # After logging in, keep this menu at the top of the screen for menu choice
 # Add logout option, screen destroyed
 # Idea to keep this portion of the game simple as possible.
@@ -24,3 +26,56 @@ print('''
           ###########################
       ''')
     # Hacking Menu :  Tool's that are available
+
+
+
+
+
+
+# Hacks
+
+print("Scanning Wifi Networks...")
+    success = random.randint(1, 100)
+    if success > 20:
+        print("Accessible Wifi Network Found")
+    elif success <= 10:
+        detection_chance += 5
+        print("You were detected. IP address has been logged and banned.")
+    else:
+        print("Failed to locate network.")
+        return MainMenu()
+
+    print("Scrubbing Hard Drive for Project Galaxy Files...")
+    success = random.randint(1, 100)
+    if success > 30:
+        print("Project Galaxy Files Found. Password Required.")
+    elif success <= 10:
+        detection_chance += 10
+        print("You were detected. IP address has been logged and banned.")
+    else:
+        print("Failed to locate Project Galaxy Files.")
+        return MainMenu()
+
+    print("Cracking Password...")
+    success = random.randing(1, 100)
+    if success > 40:
+        print("Password Cracked. Files Downloaded. Preparing Virus Injection for escape.")
+    elif success <= 10:
+        detection_chance += 15
+        print("You were detected. IP address has been logged and banned.")
+    else:
+        print("Password Cracking Failed.")
+        return MainMenu()
+
+    print("Injecting Virus into Mainframe...")
+    success = random.randint(1, 100)
+    if success > 50:
+        print("Virus Injected. Files Downloaded. Contract Complete.")
+        balance += 1000
+        print("You have been paid $1000.")
+    elif success <= 10:
+        detection_chance += 20
+        print("You were detected. IP address has been logged and banned.")
+    else:
+        print("Virus Injection Failed.")
+        return MainMenu()
--- HackingGameFunctions.py
+++ HackingGameFunctions.py
@@ -263,51 +263,7 @@ def StellarSolutions():  # Created Stellar Solutions functions baseline for cont
     print(f"{user_name}, {password} accepted")
     print(''' Welcome to Stellar Solutions! Where dreams become reality! ''')
 
-    print("Scanning Wifi Networks...")
-    success = random.randint(1, 100)
-    if success > 20:
-        print("Accessible Wifi Network Found")
-    elif success <= 10:
-        detection_chance += 5
-        print("You were detected. IP address has been logged and banned.")
-    else:
-        print("Failed to locate network.")
-        return MainMenu()
 
-    print("Scrubbing Hard Drive for Project Galaxy Files...")
-    success = random.randint(1, 100)
-    if success > 30:
-        print("Project Galaxy Files Found. Password Required.")
-    elif success <= 10:
-        detection_chance += 10
-        print("You were detected. IP address has been logged and banned.")
-    else:
-        print("Failed to locate Project Galaxy Files.")
-        return MainMenu()
-
-    print("Cracking Password...")
-    success = random.randing(1, 100)
-    if success > 40:
-        print("Password Cracked. Files Downloaded. Preparing Virus Injection for escape.")
-    elif success <= 10:
-        detection_chance += 15
-        print("You were detected. IP address has been logged and banned.")
-    else:
-        print("Password Cracking Failed.")
-        return MainMenu()
-
-    print("Injecting Virus into Mainframe...")
-    success = random.randint(1, 100)
-    if success > 50:
-        print("Virus Injected. Files Downloaded. Contract Complete.")
-        balance += 1000
-        print("You have been paid $1000.")
-    elif success <= 10:
-        detection_chance += 20
-        print("You were detected. IP address has been logged and banned.")
-    else:
-        print("Virus Injection Failed.")
-        return MainMenu()
 
 
 def GlobalBank():  # Created Global Bank function baseline for contracts
